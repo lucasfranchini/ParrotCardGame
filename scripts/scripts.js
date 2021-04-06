f0ad6d94d9be9e2f540e54b2e8284b0b1624d634
Lucas Franchini <lucasfranchini.a@gmail.com>
2021-04-06 17:16:50 -0300

    implementação do virar da carta

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,17 @@
-let numero_cartas =0;
+let numero_cartas =6;
 const deck = document.querySelector(".campo");
 while(numero_cartas<4 || numero_cartas>14 || numero_cartas%2 === 1){
     numero_cartas = parseInt(prompt("com quantas cartas você quer jogar"));
 }
 for (let i=0;i<numero_cartas;i++){
     deck.innerHTML = `${deck.innerHTML}  
-    <div class="carta">
-    <img src="imagens/front.png" alt="um papagaio" class="frente" >
-    <img src="imagens/bobrossparrot.gif" alt="um papagaio de afro" class="verso">
+    <div class="carta" onclick="virar(${i})">
+    <img src="imagens/front.png" alt="um papagaio" class="frente">
+    <img src="imagens/bobrossparrot.gif" alt="um papagaio de afro">
     </div>`
+}
+const cartas = document.querySelectorAll(".campo .carta");
+function virar(i){
+    cartas[i].querySelectorAll("img")[0].classList.toggle("frente");
+    cartas[i].querySelectorAll("img")[1].classList.toggle("frente");
 }
