bdd8043b94e2627ff7643dafdc53781e22490cf0
Lucas Franchini <lucasfranchini.a@gmail.com>
2021-04-06 23:26:39 -0300

    criação de uma função para recomeçar o jogo após o fim

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,30 +1,34 @@
-let numero_cartas =14, numero_viradas =0,acertos =0,numero_jogadas =0;
+let numero_cartas =0, numero_viradas =0,acertos =0,numero_jogadas =0;
 let i_viradas =[];
 let timer=0;
 const deck = document.querySelector(".campo");
-while(numero_cartas<4 || numero_cartas>14 || numero_cartas%2 === 1){
-    numero_cartas = parseInt(prompt("com quantas cartas você quer jogar"));
-}
-for (let i=0;i<numero_cartas;i++){
-    deck.innerHTML = `${deck.innerHTML}  
-    <div class="carta" onclick="virar(${i})">
-        <div class="frente face">
-            <img src="imagens/front.png" alt="um papagaio" >
-        </div>
-        <div class="face">
-            <img src="">
-        </div>
-    </div>`
-}
-const cartas = document.querySelectorAll(".campo .carta");
-
-for (let i=0,imagen_escolhida = 1;i<numero_cartas;i++){
-    if(i%2 ===0 && i!== 0){
-        imagen_escolhida++;
+let clock;
+iniciar();
+function iniciar (){
+    while(numero_cartas<4 || numero_cartas>14 || numero_cartas%2 === 1){
+        numero_cartas = parseInt(prompt("com quantas cartas você quer jogar"));
     }
-    cartas[i].querySelectorAll("img")[1].setAttribute("src",`imagens/${imagen_escolhida}.gif`)
+    for (let i=0;i<numero_cartas;i++){
+        deck.innerHTML = `${deck.innerHTML}  
+        <div class="carta" onclick="virar(${i})">
+            <div class="frente face">
+                <img src="imagens/front.png" alt="um papagaio" >
+            </div>
+            <div class="face">
+                <img src="">
+            </div>
+        </div>`
+    }
+    const cartas = document.querySelectorAll(".campo .carta");
+
+    for (let i=0,imagen_escolhida = 1;i<numero_cartas;i++){
+        if(i%2 ===0 && i!== 0){
+            imagen_escolhida++;
+        }
+        cartas[i].querySelectorAll("img")[1].setAttribute("src",`imagens/${imagen_escolhida}.gif`)
+    }
+    clock =setInterval(relogio,1000);
 }
-let clock =setInterval(relogio,1000);
 function relogio(){
     timer++;
     const relogio = document.querySelector(".relogio")
@@ -33,6 +37,7 @@ function relogio(){
     relogio.innerHTML = `${minutos}:${segundos}`;
 }
 function virar(i){
+    const cartas = document.querySelectorAll(".campo .carta");
     i_viradas[numero_viradas]=i;
     numero_viradas ++;
     numero_jogadas++;
@@ -56,10 +61,25 @@ function virar(i){
         {
             clearInterval(clock);
             alert(`você ganhou em ${numero_jogadas} jogadas e demorou ${timer} segundos` );
+            recomeçar();
         }
     }
 }
+function recomeçar(){
+    const resposta =prompt("gostaria de jogar de novo?(sim ou não)");
+    if(resposta === "sim"){
+        numero_cartas =0;
+        numero_viradas =0;
+        acertos =0;
+        numero_jogadas =0;
+        i_viradas =[];
+        timer=0;
+        deck.innerHTML = "";
+        iniciar();
+      }
+}
 function desvirar (viradas,i_viradas){
+    const cartas = document.querySelectorAll(".campo .carta");
     viradas[0].classList.remove("virada");
     viradas[1].classList.remove("virada");
     cartas[i_viradas[0]].querySelectorAll(".face")[0].classList.toggle("frente");
